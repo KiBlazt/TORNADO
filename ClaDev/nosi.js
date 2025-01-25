841fd039e20e4fc93155bca3c9390bc55b4410ed
pikachu353 <davidpajuegos007@gmail.com>
2025-01-25 10:51:30 -0600

    0.2

---
+++ ClaDev/nosi.js
@@ -0,0 +1,10 @@
+function alerta(){
+    alert("se han guardado tus datos correctamente");
+}
+function cambiar(){
+    document.getElementById("guardarBTN").innerHTML;
+}
+document.getElementById("guardarBTN").onclick = function(){
+    alerta();
+    cambiar();      
+}
